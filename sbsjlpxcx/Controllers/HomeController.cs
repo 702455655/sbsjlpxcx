f5029b6aff80b89bf92aecdf47a65766fed30ed4
帅笔 <702455655@qq.com>
2017-06-11 12:28:29 +0800

    添加用户界面

--- sbsjlpxcx/Controllers/HomeController.cs
+++ sbsjlpxcx/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using sbsjlpxcx.Busines;
-using System.Web.Mvc;
-using sbsjlpxcx.Entity._02_Parms;
-using sbsjlpxcx.Entity._01_Models;
+﻿using System.Web.Mvc;
 
 namespace sbsjlpxcx.Controllers
 {
@@ -16,30 +13,6 @@ namespace sbsjlpxcx.Controllers
         {
             return View("Index");
         }
-        /// <summary>
-        /// 只是测试能否拿到数据
-        /// </summary>
-        /// <param name="parms">前台传回来的参数</param>
-        /// <returns></returns>
-        public ActionResult GetUser(Sys_UserParms parms)
-        {
-            parms.PageIndex = 1;
-            parms.PageSize = 10;
-            return Json(new Sys_UserBLL().GetSys_UserList(parms));
-        }
-        public ActionResult AddUser()
-        {
-            Sys_User user = new Sys_User();
-            user.Name = "大傻逼";
-            user.Password = "123456";
-            user.Usertype = false;
-            return Content(new Sys_UserBLL().Insert(user).ToString());
-
-        }
-
-        public ActionResult GetUserInfo(Sys_UserParms parms)
-        {
-            return Json(new Sys_UserBLL().GetSys_User(parms));
-        }
+        
     }
 }
---
+++ sbsjlpxcx/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using sbsjlpxcx.Busines;
+using sbsjlpxcx.Entity._01_Models;
+using sbsjlpxcx.Entity._02_Parms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sbsjlpxcx.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 只是测试能否拿到数据
+        /// </summary>
+        /// <param name="parms">前台传回来的参数</param>
+        /// <returns></returns>
+        public ActionResult GetUser(Sys_UserParms parms)
+        {
+            parms.PageIndex = 1;
+            parms.PageSize = 10;
+            return Json(new Sys_UserBLL().GetSys_UserList(parms));
+        }
+        public ActionResult AddUser()
+        {
+            Sys_User user = new Sys_User();
+            user.Name = "大傻逼";
+            user.Password = "123456";
+            user.Usertype = false;
+            return Content(new Sys_UserBLL().Insert(user).ToString());
+
+        }
+
+        public ActionResult GetUserInfo(Sys_UserParms parms)
+        {
+            return Json(new Sys_UserBLL().GetSys_User(parms));
+        }
+    }
+}
